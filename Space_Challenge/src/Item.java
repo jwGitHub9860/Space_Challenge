8d1bc4e4c11a8ea23ba04ce8c80a636cf0f04a39
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 18:46:24 -0400

    refactor: Refactoring production code
    Replace "static" with "public" and delete comments about "static" in "Rocket" class of "Item.java" java class

--- Space_Challenge/src/Item.java
+++ Space_Challenge/src/Item.java
@@ -2,7 +2,7 @@ public class Item {
     String name;
     int weight;
     public class Rocket implements Spaceship {
-        static boolean launch(Simulation simulation){ // indicates if "launch" was Successful or Not    MUST BE "static boolean" NOT "boolean static" or Error will occur
+        public boolean launch(Simulation simulation){ // indicates if "launch" was Successful or Not
             if (simulation.test == 10){ //  MUST if STATEMENT CONDITION
                 return true;
             }
@@ -10,7 +10,7 @@ public class Item {
                 return false;
             }
         }
-        static boolean land(Simulation simulation){ // indicates if "land" was Successful or Not through "launch" result
+        public boolean land(Simulation simulation){ // indicates if "land" was Successful or Not through "launch" result
             if (simulation.test == 10){ //  MUST if STATEMENT CONDITION
                 return true;
             }
@@ -18,7 +18,7 @@ public class Item {
                 return false;
             }
         }
-        static boolean canCarry(Simulation simulation){
+        public boolean canCarry(Simulation simulation){
             if (simulation.test == 10){ //  MUST if STATEMENT CONDITION
                 return true;
             }
@@ -26,12 +26,12 @@ public class Item {
                 return false;
             }
         }
-        static int carry(Item item){ // updates current weight
+        public int carry(Item item){ // updates current weight
             return item.weight++;
         }
     }
     public interface Spaceship {
-        boolean launch(Simulation simulation); // indicates if "launch" was Successful or Not    MUST BE "static boolean" NOT "boolean static" or Error will occur
+        boolean launch(Simulation simulation); // indicates if "launch" was Successful or Not
         boolean land(Simulation simulation); // indicates if "land" was Successful or Not through "launch" result
         boolean canCarry(Simulation simulation);
         int carry(Item item); // updates current weight
