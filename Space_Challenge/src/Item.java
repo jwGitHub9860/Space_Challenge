2f4f3aa1e06d53295fec660d7b5359443ea441d8
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 18:42:46 -0400

    refactor: Refactoring production code
    Replace "public interface Spaceship {" code line with "public class Rocket implements SpaceShip {" code line and move "public interface Spaceship" below "Rocket" class in "Item" class

--- Space_Challenge/src/Item.java
+++ Space_Challenge/src/Item.java
@@ -1,7 +1,7 @@
 public class Item {
     String name;
     int weight;
-    public interface Spaceship {
+    public class Rocket implements Spaceship {
         static boolean launch(Simulation simulation){ // indicates if "launch" was Successful or Not    MUST BE "static boolean" NOT "boolean static" or Error will occur
             if (simulation.test == 10){ //  MUST if STATEMENT CONDITION
                 return true;
@@ -29,5 +29,11 @@ public class Item {
         static int carry(Item item){ // updates current weight
             return item.weight++;
         }
+    }
+    public interface Spaceship {
+        boolean launch(Simulation simulation); // indicates if "launch" was Successful or Not    MUST BE "static boolean" NOT "boolean static" or Error will occur
+        boolean land(Simulation simulation); // indicates if "land" was Successful or Not through "launch" result
+        boolean canCarry(Simulation simulation);
+        int carry(Item item); // updates current weight
     }
 }
