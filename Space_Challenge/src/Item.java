d9e7a650889c1e3d4b305b17015236ac67455b0e
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 19:35:46 -0400

    refactor: Refactoring production code
    Create "U1" and "U2" classes in "Item.java" java class

--- Space_Challenge/src/Item.java
+++ Space_Challenge/src/Item.java
@@ -36,4 +36,18 @@ public class Item {
         boolean canCarry(Simulation simulation);
         int carry(Item item); // updates current weight
     }
+    public class U1 extends Rocket{
+        int cost = 100; // in Millions $
+        int weight = 10; // Tonnes
+        int max_weight = 18; // Tonnes (with cargo)
+        int launch_explosion_chance = 5; // % * (cargo carried / cargo limit)
+        int landing_crash_chance = 1; // % * (cargo carried / cargo limit)
+    }
+    public class U2 extends Rocket{
+        int cost = 120; // in Millions $
+        int weight = 18; // Tonnes
+        int max_weight = 29; // Tonnes (with cargo)
+        int launch_explosion_chance = 4; // % * (cargo carried / cargo limit)
+        int landing_crash_chance = 8; // % * (cargo carried / cargo limit)
+    }
 }
