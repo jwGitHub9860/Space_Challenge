e4978502732a0207af577867dd774de687bcd2b8
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 18:13:48 -0400

    test: Adding tests, refactoring test
    Add boolean "land" method with to "Spaceship" interface in "Item" class

--- Space_Challenge/src/Item.java
+++ Space_Challenge/src/Item.java
@@ -1,14 +1,22 @@
 public class Item {
     String name;
     int weight;
-    public interface Space {
-        static boolean launch(Simulation simulation){ // MUST BE "static boolean" NOT "boolean static" or Error will occur
-            if (simulation.test == 10){ // indicates if launch was Successful or Not   MUST if STATEMENT CONDITION
+    public interface Spaceship {
+        static boolean launch(Simulation simulation){ // indicates if "launch" was Successful or Not    MUST BE "static boolean" NOT "boolean static" or Error will occur
+            if (simulation.test == 10){ //  MUST if STATEMENT CONDITION
                 return true;
             }
             else{
                 return false;
             }
         };
+        static boolean land(Simulation simulation){ // indicates if "land" was Successful or Not through "launch" result
+            if (simulation.test == 10){ //  MUST if STATEMENT CONDITION
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
     }
 }
