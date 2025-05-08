a6bc22b9594837f7cb637b31ecb1fbf131acd5b7
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 20:20:26 -0400

    refactor: Refactoring production code
    Add code to read "Phase-1" and "Phase-2" text files in "Main.java" java class

--- Space_Challenge/src/Main.java
+++ Space_Challenge/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.RandomAccessFile;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,6 +7,9 @@ public class Main {
         // Access "Phase-1" and "Phase-2" text files
         File file1 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-1"); // access "Phase-1" text file
         File file2 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-2"); // access "Phase-2" text file
+        final RandomAccessFile phase1 = new RandomAccessFile(file1, "r"); // creates a random access file stream to read from
+        final RandomAccessFile phase2 = new RandomAccessFile(file2, "r"); // creates a random access file stream to read from
+
         System.out.printf("(" + simulation.test + "," + Simulation.test2 + ")"); // TESTING CODE
 
         for (int i = 1; i <= 5; i++) {
