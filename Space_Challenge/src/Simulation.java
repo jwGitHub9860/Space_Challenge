6971684bbef4a68c384edaa81af03601451bd9bb
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 22:48:26 -0400

    fix: A bug fix
    Uncomment print statement testing code in "loadItems" method in "Simulation.java" java file

--- Space_Challenge/src/Simulation.java
+++ Space_Challenge/src/Simulation.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 public class Simulation {
     Item item = new Item(); // creates "Items" object to access "Item.java" java file
     public static ArrayList<Integer> loadItems(File file1, File file2) {
-        System.out.printf("text file test: " + test + "\n"); // TESTING CODE
+        //System.out.printf("text file test: " + test + "\n"); // TESTING CODE
     }
 }
