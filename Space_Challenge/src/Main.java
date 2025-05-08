db6b84771f4442968a41ed46b3e9ff8b491b4cac
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 23:03:41 -0400

    refactor: Refactoring production code
    Copy and paste code for reading "file1" and "file2" from "Main.java" java class into "loadItems" method in "Simulation.java" java file and add exception to "loadItems" method

--- Space_Challenge/src/Main.java
+++ Space_Challenge/src/Main.java
@@ -9,8 +9,8 @@ public class Main {
         // Access "Phase-1" and "Phase-2" text files
         File file1 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-1.txt"); // access "Phase-1" text file
         File file2 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-2.txt"); // access "Phase-2" text file
-        final RandomAccessFile phase1 = new RandomAccessFile(file1, "r"); // creates a random access file stream to read from
-        final RandomAccessFile phase2 = new RandomAccessFile(file2, "r"); // creates a random access file stream to read from
+        //final RandomAccessFile phase1 = new RandomAccessFile(file1, "r"); // creates a random access file stream to read from
+        //final RandomAccessFile phase2 = new RandomAccessFile(file2, "r"); // creates a random access file stream to read from
 
         Simulation.loadItems(file1, file2); // calls "loadItems" method
 
--- Space_Challenge/src/Simulation.java
+++ Space_Challenge/src/Simulation.java
@@ -1,10 +1,13 @@
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
 public class Simulation {
     Item item = new Item(); // creates "Items" object to access "Item.java" java file
-    public static ArrayList<Integer> loadItems(File file1, File file2) {
-
+    public static ArrayList<Integer> loadItems(File file1, File file2) throws IOException {
+        final RandomAccessFile phase1 = new RandomAccessFile(file1, "r"); // creates a random access file stream to read from
+        final RandomAccessFile phase2 = new RandomAccessFile(file2, "r"); // creates a random access file stream to read from
         //System.out.printf("text file test: " + test + "\n"); // TESTING CODE
     }
 }
