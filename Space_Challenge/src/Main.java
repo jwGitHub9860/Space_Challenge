207bae01b0cf3b24eb9598ac843e09507b8de2ac
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 20:14:35 -0400

    refactor: Refactoring production code
    Add comment marking testing code in "Main.java" java class

--- Space_Challenge/src/Main.java
+++ Space_Challenge/src/Main.java
@@ -6,11 +6,11 @@ public class Main {
         // Access "Phase-1" and "Phase-2" text files
         File file1 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-1"); // access "Phase-1" text file
         File file2 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-2"); // access "Phase-2" text file
-        System.out.printf("(" + simulation.test + "," + Simulation.test2 + ")");
+        System.out.printf("(" + simulation.test + "," + Simulation.test2 + ")"); // TESTING CODE
 
         for (int i = 1; i <= 5; i++) {
 
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
             System.out.println("i = " + i);
         }
     }
