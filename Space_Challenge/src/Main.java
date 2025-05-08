c90b3aef45ae2e09657f185f9e39324769c5945e
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 21:33:56 -0400

    fix: A bug fix
    Change pathname for "file1" and "file2" in "Main.java" java file

--- Space_Challenge/src/Main.java
+++ Space_Challenge/src/Main.java
@@ -7,8 +7,8 @@ public class Main {
         Simulation simulation = new Simulation(); // access "Simulation.java" java file
 
         // Access "Phase-1" and "Phase-2" text files
-        File file1 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-1"); // access "Phase-1" text file
-        File file2 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-2"); // access "Phase-2" text file
+        File file1 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-1.txt"); // access "Phase-1" text file
+        File file2 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-2.txt"); // access "Phase-2" text file
         final RandomAccessFile phase1 = new RandomAccessFile(file1, "r"); // creates a random access file stream to read from
         final RandomAccessFile phase2 = new RandomAccessFile(file2, "r"); // creates a random access file stream to read from
 
