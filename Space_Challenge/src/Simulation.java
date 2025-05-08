84f54aecded3bf47f6dadc4f160d58e0663d308a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-08 01:05:53 -0400

    refactor: Refactoring production code
    Change "loadItems" method from "static void" to "ArrayList<String>", delete first four lines of code with ".readLine()" in "Simulation.java" java file

--- Space_Challenge/src/Simulation.java
+++ Space_Challenge/src/Simulation.java
@@ -5,14 +5,10 @@ import java.util.ArrayList;
 
 public class Simulation {
     Item item = new Item(); // creates "Items" object to access "Item.java" java file
-    public static void loadItems(File file1, File file2) throws IOException {
+    public ArrayList<String> loadItems(File file1, File file2) throws IOException {
         // Reads data in "Phase-1" and "Phase-2" text files
         final RandomAccessFile phase1 = new RandomAccessFile(file1, "r"); // creates a random access file stream to read from
         final RandomAccessFile phase2 = new RandomAccessFile(file2, "r"); // creates a random access file stream to read from
-        phase1.readLine(); // reads data in "Phase-1" text files (need because First line read does NOT have whole item)
-        phase2.readLine(); // reads data in "Phase-2" text files (need because First line read does NOT have whole item)
-        String rocket1_item = phase1.readLine(); // reads data in "Phase-1" text files
-        String rocket2_item = phase2.readLine(); // reads data in "Phase-2" text files
 
         // Create ArrayLists for rockets
         ArrayList<String> rocket1 = new ArrayList<String>();
@@ -22,6 +18,9 @@ public class Simulation {
         String line;
         while ((line = phase1.readLine()) != null) {
             rocket1.add(line); // adds data from "Phase-1" text file to "rocket1" array list
+        }
+        while ((line = phase2.readLine()) != null) {
+            rocket2.add(line); // adds data from "Phase-1" text file to "rocket1" array list
         }
 
         System.out.println("rocket1:"); // TESTING CODE
