56b8ec2cedbb3108c51ebeba9f595952077a6cd5
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-08 00:12:45 -0400

    test: Adding tests, refactoring test
    Comment out testing code in while loop in "loadItems" method in "Simulation.java" java file

--- Space_Challenge/src/Simulation.java
+++ Space_Challenge/src/Simulation.java
@@ -21,12 +21,12 @@ public class Simulation {
         // Reads data in "Phase-1" and "Phase-2" text files
         String line;
         while ((line = phase1.readLine()) != null) {
-            System.out.println(line); // TESTING CODE
+            //System.out.println(line); // TESTING CODE
             rocket1.add(line); // adds data from "Phase-1" text file to "rocket1" array list
         }
 
         for (String data : rocket1){ //
-            System.out.printf("text file test: " + phase1 + "\n"); // TESTING CODE
+            System.out.printf("text file test: " + data + "\n"); // TESTING CODE
         }
         for (String data : rocket2){ //
             System.out.printf("text file test: " + data + "\n"); // TESTING CODE
