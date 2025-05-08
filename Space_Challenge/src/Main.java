674ecf63adc064e49014084f32343ed789e64fe6
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 20:22:24 -0400

    fix: A bug fix
    Add exception in case text files fail to open in "Main.java" java class

--- Space_Challenge/src/Main.java
+++ Space_Challenge/src/Main.java
@@ -1,9 +1,11 @@
 import java.io.File;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException { // if file CANNOT be opened
         Simulation simulation = new Simulation(); // access "Simulation.java" java file
+
         // Access "Phase-1" and "Phase-2" text files
         File file1 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-1"); // access "Phase-1" text file
         File file2 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-2"); // access "Phase-2" text file
