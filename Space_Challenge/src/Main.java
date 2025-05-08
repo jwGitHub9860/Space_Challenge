2b480019a4a353521d1a5719fcd85bba2bc7e84b
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-07 20:05:13 -0400

    refactor: Refactoring production code
    Add code to access "Phase-1" and "Phase-2" text files and comment for accessing "Simulation.java" java class in "Main.java" java class

--- Space_Challenge/src/Main.java
+++ Space_Challenge/src/Main.java
@@ -1,12 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        Simulation simulation = new Simulation();
+        Simulation simulation = new Simulation(); // access "Simulation.java" java file
+        // Access "Phase-1" and "Phase-2" text files
+        File file1 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-1"); // access "Phase-1" text file
+        File file2 = new File("C:\\Users\\jwori\\JavaProjects\\Phase-2"); // access "Phase-2" text file
         System.out.printf("(" + simulation.test + "," + Simulation.test2 + ")");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
