3a80b220d18e3ab9927dc5ef55c310a086e70edd
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-10 12:42:08 -0400

    fix: A bug fix
    Add "landSuccess" boolean initialization below "launchSuccess" initialization to while loop in "runSimulation" function in "Simulation.java" java file

--- Space_Challenge/src/Simulation.java
+++ Space_Challenge/src/Simulation.java
@@ -70,6 +70,7 @@ public class Simulation {
     public static double runSimulation(ArrayList<Integer>rocketWeights, double rocketBudgetTotal){
         Item item = new Item(); // creates "Items" object to access "Item.java" java file
         boolean launchSuccess = false; // confirms if rocket launch was successful
+        boolean landSuccess = false; // confirms if rocket landing was successful
         System.out.println("launchSuccess: " + launchSuccess); // TESTING CODE
         System.out.println("\nrocketWeights.size(): " + rocketWeights.size()); // TESTING CODE
         for (int num = 0; num < rocketWeights.size(); num++) {
